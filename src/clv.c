f2f9c95c7a5d75bee8293b5e4465eb3e41c27e34
44mira <legolaslada@gmail.com>
2024-11-15 00:30:03 +0800

    feat: source code for ASSERT and related macros

---
+++ src/clv.c
@@ -0,0 +1,51 @@
+#include "clv.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char clvPass[] = "\x1b[42;30m  OK  \x1b[0m";
+static const char clvFail[] = "\x1b[41;30m FAIL \x1b[0m";
+static uint testCount = 0;
+char *testLabel;
+
+void CLV_SETUP() {
+  // dynamically allocate testLabel
+  testLabel = (char *)malloc(LABEL_SIZE * sizeof testLabel);
+
+  // initialize testLabel
+  TEST_LABEL("");
+}
+void CLV_TEARDOWN() { free(testLabel); }
+void CLV_UPDATE_LABEL(char *label) { strcpy(testLabel, label); }
+
+/* Asserts ---------------------------------------------------*/
+
+void CLV_ASSERT(CLV_BOOL condition, uint line) {
+  testCount++;
+
+  if (condition) {
+    CLV_PASS(line);
+  } else {
+    CLV_FAIL(line);
+  }
+
+  // Invalidate label
+  TEST_LABEL("");
+}
+
+void CLV_PASS(uint line) {
+  if (strcmp(testLabel, "") == 0) {
+    printf("%s | TEST #%02u LINE %-5u\n", clvPass, testCount, line);
+  } else {
+    printf("%s | TEST #%02u LINE %-5u | %s\n", clvPass, testCount, line,
+           testLabel);
+  }
+}
+
+void CLV_FAIL(uint line) {
+  if (strcmp(testLabel, "") == 0) {
+    printf("%s | TEST #%02u LINE %-5u\n", clvFail, testCount, line);
+  } else {
+    printf("%s | TEST #%02u LINE %-5u | " TEXT_RED("%s") "\n", clvFail,
+           testCount, line, testLabel);
+  }
+}
