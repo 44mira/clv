d423a888a571e1ad448af6417dd8a8f5017f5ed9
44mira <legolaslada@gmail.com>
2024-11-17 01:46:57 +0800

    feat: clv binary

---
+++ clv.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+//go:embed src/*
+var src embed.FS
+
+func writeFile(path, content string) error {
+	os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	return os.WriteFile(path, []byte(content), 0644)
+}
+
+// [[ createTestDir ]] {{{
+func createTestDir() error {
+	files := []string{"src/clv.c", "src/clv.h", "src/clv_internals.h"}
+
+	for _, file := range files {
+		content, err := src.ReadFile(file)
+		if err != nil {
+			return err
+		}
+
+		target := filepath.Join("tests", filepath.Base(file))
+
+		err = writeFile(target, string(content))
+		if err != nil {
+			return err
+		}
+	}
+
+	content, err := src.ReadFile("src/Makefile")
+	if err != nil {
+		return err
+	}
+
+	err = writeFile("Makefile", string(content))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// }}}
+
+// [[ createTests ]] {{{
+func createTests() error {
+	template, err := src.ReadFile("src/template.c")
+	if err != nil {
+
+		return err
+	}
+
+	for i := 2; i < len(os.Args); i++ {
+		target := filepath.Join("tests", os.Args[i])
+
+		err = writeFile(target+"_test.c", string(template))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// }}}
+
+func displayHelp() {
+	fmt.Printf(`
+    c'est la vie
+
+    clv init                                    -- initialize the tests/ directory
+    clv help                                    -- display this menu
+    clv create <test_name> [ ... <test_name> ]  -- create a test template
+    `)
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		err := createTestDir()
+		if err != nil {
+			log.Fatalf("An error has occurred in creating the tests/ directory: %v\n", err.Error())
+		}
+
+		fmt.Println("The tests/ directory has been created.")
+		return
+	}
+
+	switch os.Args[1] {
+	case "create":
+		if len(os.Args) < 3 {
+			log.Fatalln("Please provide the test name(s).")
+		}
+
+		err := createTests()
+		if err != nil {
+			log.Fatalf("An error has occurred in creating the test template(s): %v\n", err.Error())
+		}
+		fmt.Println("The tests have been created.")
+	case "help":
+		displayHelp()
+	default:
+		displayHelp()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/44mira/clv
+
+go 1.23.3
